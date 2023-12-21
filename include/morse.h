347ce4153e66a1c76b6542dfca86ff63bf548590
Aust2000 <felipeuberaba10@gmail.com>
2023-12-21 18:46:38 -0300

    prototipo algoritmo audio morse

--- include/morse.h
+++ include/morse.h
@@ -1,7 +1,13 @@
 #ifndef MORSE
 #define MORSE
 
+// Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// Protótipos de Funções
+void morseAudio(char *morse);
 
 #endif
--- src/functions.c
+++ src/functions.c
@@ -1,1 +1,20 @@
 #include "../include/morse.h"
+
+void morseAudio(char *morse) {
+    int tam;
+    for (int i = 0; i < strlen(morse); i++) {
+        if (morse[i] == ' ') {
+            _sleep(3000);
+        } else if (morse[i] == '-') {
+            tam = 2000;
+        } else {
+            tam = 1000;
+        }
+
+        while (tam--) {
+            printf("%c", 7);
+        }
+
+        _sleep(1000);
+    }
+}
