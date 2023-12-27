56014513e94ac785d8a4d1efd5e2ae9fb74752fe
emyllisoares <100449171+emyllisoares@users.noreply.github.com>
2023-12-27 11:57:49 -0300

    interface

--- interface.c
+++ interface.c
@@ -1,4 +1,4 @@
-//funçoes alfanumerico para morse e morse para alfanumerico
+//funçoes alfanumerico para morse e morse para alfanumerico.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
