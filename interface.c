30bc4e7c092f0bb3b9efcbe4dbd90dc135933c08
emyllisoares <100449171+emyllisoares@users.noreply.github.com>
2023-12-28 16:48:40 +0000

    interface

--- interface.c
+++ interface.c
@@ -14,6 +14,45 @@ void clear(){
         system("cls");
     #endif
 }
+void conv_MorseParaAlfaNumerico(char *morse){
+    char *alfabeto[27] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "/"};
+    char *numeros[10] = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
+    char *caractere = strtok((char *)morse, " ");
+    
+    while (caractere != NULL){
+        int i;
+        int encontrado = 0;
+
+        for (i = 0; i < 27; i++) {
+            if (strcmp(caractere, alfabeto[i]) == 0) {
+                if (i == 26) {
+                    printf(" ");
+                    encontrado = 1;
+                    break;
+                } else {
+                    printf("%c", 'A' + i);
+                    encontrado = 1;
+                    break;
+                }
+            }
+        }
+
+        for (i = 0; i < 10; i++) {
+            if (strcmp(caractere, numeros[i]) == 0) {
+                printf("%d", i);
+                encontrado = 1;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            printf("(?)");
+        }
+
+        caractere = strtok(NULL, " ");
+    }
+}
 
 void menu() {
     printf(ROXO"\t\t========= Conversor Morse =========\n\n"FIM_COR);
