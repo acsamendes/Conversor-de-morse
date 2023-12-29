7e22566dee99680a073fae049eca30b82cd9fcae
Felipe Sucupira <58998427+Aust2000@users.noreply.github.com>
2023-12-29 22:36:22 +0000

    algoritmo reprodução morse audio completo na teoria

--- include/morse.h
+++ include/morse.h
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+
 
 // Protótipos de Funções
 void morseAudio(char *morse);
--- src/functions.c
+++ src/functions.c
@@ -6,17 +6,22 @@ void morseAudio(char *morse) {
     int tam;
     for (int i = 0; i < strlen(morse); i++) {
         if (morse[i] == ' ') {
-            _sleep(3000);
+            sleep(2);
         } else if (morse[i] == '-') {
             tam = 2000;
         } else {
             tam = 1000;
         }
 
-        while (tam--) {
-            printf("%c", 7);
-        }
+        #ifdef __linux__
+            while (tam--) {
+                printf("\a");
+            }
+        #elif _WIN32
+            BEEP(800, tam);
+        #endif
 
-        _sleep(1000);
+        sleep(1);
+        tam = 0;
     }
 }
