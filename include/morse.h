ef09793dd1011bf70d098237fbde1f10c550af81
Felipe Sucupira <58998427+Aust2000@users.noreply.github.com>
2023-12-29 17:05:30 +0000

    adicionados protótipos de funções da interface

--- include/morse.h
+++ include/morse.h
@@ -1,6 +1,7 @@
 #ifndef MORSE
 #define MORSE
 
+
 // Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,13 +10,19 @@
 #include <string.h>
 #include <ctype.h>
 
+
 // Variáveis Globais
 #define ROXO "\e[1;34m"
 #define FIM_COR "\e[0m"
+
 
 // Protótipos de Funções
 void lerPorClique(char *palavra);
 
 void conv_MorseParaAlfaNumerico(char *morse);
+
+void clear();
+void menu();
+void esperar();
 
 #endif
